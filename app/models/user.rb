2fcf8e9c00b186115410fa68db0c8ed3da6073d0
Mary Seo <maryseo070@gmail.com>
2018-03-05 22:01:04 -0500

    start backend auth

---
+++ app/models/user.rb
@@ -0,0 +1,34 @@
+class User < ApplicationRecord
+  validates :username, :password_digest, :session_token, presence: true
+  validates :username, uniqueness: true
+  validates :password, length: { minimum: 6, allow_nil: true }
+
+  after_initialize :ensure_session_token
+  attr_reader :password
+
+  def reset_session_token!
+    self.session_token = SecureRandom.urlsafe_base64
+    self.save!
+    self.session_token
+  end
+
+  def password=(password)
+    @password = password
+    self.password_digest = BCrypt::Password.create(password)
+  end
+
+  def is_password?(password)
+    BCrypt::Password.new(self.password_digest).is_password?(password)
+  end
+
+  def ensure_session_token
+    self.session_token ||= SecureRandom.urlsafe_base64
+  end
+
+  def self.find_by_credentials(username password)
+    user = User.find_by(username: username)
+    return user && user.is_password?(password) ? user : nil
+  end
+
+
+end
---
+++ frontend/actions/session_actions.js
@@ -0,0 +1,33 @@
+import * as APIUtil from '../util/session_api_util';
+export const RECEIVE_CURRENT_USER = 'RECEIVE_CURRENT_USER';
+export const RECEIVE_SESSION_ERRORS = 'RECEIVE_SESSION_ERRORS';
+
+export const receiveCurrentUser = (user) =>{
+  return {
+    type: RECEIVE_CURRENT_USER,
+    user: user
+  };
+};
+
+export const receiveErrors = (errors) =>{
+  return {
+    type: RECEIVE_SESSION_ERRORS,
+    errors
+  };
+};
+
+export const signup = (user) => (dispatch) => {
+  return APIUtil.signup(user).then(
+    (user) => dispatch(receiveCurrentUser(user))
+  );
+};
+
+export const login = (user) => (dispatch) => {
+  return APIUtil.login(user).then(
+  (user) => dispatch(receiveCurrentUser(user)),
+  (errors) => dispatch(receiveErrors(errors)));
+};
+
+export const logout = () => (dispatch) => {
+  return APIUtil.logout().then(dispatch(receiveCurrentUser(null)));
+};
