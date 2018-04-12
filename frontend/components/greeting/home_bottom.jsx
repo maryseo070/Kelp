bb0e1de856055572c46b8b85d71ba71c224bdcc7
maryseo070 <maryseo070@gmail.com>
2018-04-11 20:03:14 -0400

    home page bottom styled

--- frontend/components/greeting/home_bottom.jsx
+++ frontend/components/greeting/home_bottom.jsx
@@ -5,8 +5,36 @@ class HomeBottom extends React.Component {
   constructor(props) {
     super(props);
     this.state = {};
+    this.ratingStars = this.ratingStars.bind(this);
   }
 
+ ratingStars(n) {
+    if (n === 5) {
+      return (
+        <div className='icon-five'></div>
+      );
+    }
+    else if (n === 4) {
+      return (
+        <div className='icon-four'></div>
+      );
+    }
+    else if (n === 3) {
+      return (
+        <div className='icon-three'></div>
+      );
+    }
+    else if (n === 2) {
+      return (
+        <div className='icon-two'></div>
+      );
+    }
+    else if (n === 1) {
+      return (
+        <div className='icon-one'></div>
+      );
+    }
+  }
 
 
   render (){
@@ -16,19 +44,27 @@ class HomeBottom extends React.Component {
       reefs = this.props.reefs;
       reefInfo = reefs.map((reef) => {
         return (
-          <ul>
-            <li>{reef.name}</li>
-            <li>{reef.avg_rating}</li>
-            <li>{reef.continent}</li>
-          </ul>
+            <ul className="ul-three-show-items">
+              <img src={reef.photo} className="home-item-photo"></img>
+              <div className="home-bottom-info">
+                <Link className="li-link" to={`/reefs/${reef.id}`}>{reef.name}</Link>
+                <div className="li-stars-and-reviews">
+                  <li>{this.ratingStars(Math.floor(reef.avg_rating))}</li>
+                  <li className="li-review-count">{reef.review_count} Reviews</li>
+                </div>
+                <li>{reef.continent}</li>
+              </div>
+            </ul>
       );
     });
     }
 
     return(
-      <div>
-        <h3>Kelp Manhattan</h3>
-        {reefInfo}
+      <div className="home-bottom-all">
+        <h3 className="kelp-centered">Kelp Manhattan</h3>
+        <div className="home-bottom-items">
+          {reefInfo}
+        </div>
       </div>
     );
   }
